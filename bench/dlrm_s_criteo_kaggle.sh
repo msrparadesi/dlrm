7c356532c468081170383a58066217f396589e67
Martin Paradesi <martin.paradesi@gmail.com>
2020-11-12 17:31:56 -0500

    Reset between benchmarks.

--- bench/dlrm_s_criteo_kaggle.sh
+++ bench/dlrm_s_criteo_kaggle.sh
@@ -17,16 +17,34 @@ fi
 dlrm_pt_bin="python3 dlrm_s_pytorch.py"
 dlrm_c2_bin="python3 dlrm_s_caffe2.py"
 
+date
 echo "run pytorch ..."
 # WARNING: the following parameters will be set based on the data set
 # --arch-embedding-size=... (sparse feature sizes)
 # --arch-mlp-bot=... (the input to the first layer of bottom mlp)
 $dlrm_pt_bin --arch-sparse-feature-size=16 --arch-mlp-bot="13-512-256-64-16" --arch-mlp-top="512-256-1" --data-generation=dataset --data-set=kaggle --raw-data-file=/mnt/data/dac/train.txt --processed-data-file=/mnt/data/dac/kaggleAdDisplayChallenge_processed.npz --loss-function=bce --round-targets=True --learning-rate=0.1 --mini-batch-size=128 --print-freq=1024 --print-time --test-mini-batch-size=16384 --test-num-workers=16 $dlrm_extra_option 2>&1 | tee /home/centos/run_kaggle_pt.log
+date
 
+# Reset
+echo 'resetting...'
+echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a
+rm -rf /home/centos/dlrm/__pycache__/
+rm -rf /mnt/data/dac/train_day_*
+rm -rf /mnt/data/dac/*.npz
+
+date
 echo "run caffe2 ..."
 # WARNING: the following parameters will be set based on the data set
 # --arch-embedding-size=... (sparse feature sizes)
 # --arch-mlp-bot=... (the input to the first layer of bottom mlp)
 $dlrm_c2_bin --arch-sparse-feature-size=16 --arch-mlp-bot="13-512-256-64-16" --arch-mlp-top="512-256-1" --data-generation=dataset --data-set=kaggle --raw-data-file=/mnt/data/dac/train.txt --processed-data-file=/mnt/data/dac/kaggleAdDisplayChallenge_processed.npz --loss-function=bce --round-targets=True --learning-rate=0.1 --mini-batch-size=128 --print-freq=1024 --print-time $dlrm_extra_option 2>&1 | tee /home/centos/run_kaggle_c2.log
+date
+
+# Reset
+echo 'resetting...'
+echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a
+rm -rf /home/centos/dlrm/__pycache__/
+rm -rf /mnt/data/dac/train_day_*
+rm -rf /mnt/data/dac/*.npz
 
 echo "done"
